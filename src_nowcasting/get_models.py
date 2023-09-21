28f4805c12d960cd86f1bad5caf01c262f684858
maciej-sakwa <maciej.sakwa@gmail.com>
2023-09-21 12:01:39 +0200

    SCNN cleanup

--- src_nowcasting/get_models.py
+++ src_nowcasting/get_models.py
@@ -38,88 +38,73 @@ def forecastnet(input_img_size: int, n_channels: int, n_classes: int) -> models.
     return model
 
 
-def SCNN_diff(input_shape=(128, 128, 1)) -> models.Sequential:
-    model = models.Sequential()
-    model.add(Conv2D(input_shape=input_shape, filters=64, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=64, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=128, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=128, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=256, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Flatten())
-    model.add(Dropout(0.2))
-    model.add(Dense(256, activation='linear'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1, activation='linear'))
-    # model.summary()
-    return model
-
-
 def SCNN(input_shape=(128, 128, 1)) -> models.Sequential:
-    model = models.Sequential()
-    model.add(Conv2D(input_shape=input_shape, filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Flatten())
-    model.add(Dropout(0.2))
-    model.add(Dense(256, activation='linear'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1, activation='linear'))
+    """Definition of a eCNN using keras Sequential API"""
+
+    model = models.Sequential([
+        Conv2D(input_shape=input_shape, filters=64, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+
+        Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+
+        Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+
+        Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+
+        Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"),
+        Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"),
+        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+
+        Flatten(),
+        Dropout(0.2),
+        Dense(256, activation='linear'),
+        Dropout(0.2),
+        Dense(1, activation='linear')
+    ])
+
     # model.summary()
     return model
 
 
-def scnn_classes(input_shape=(128, 128, 1), n_classes: int = 4) -> models.Sequential:
-    model = models.Sequential()
-    model.add(Conv2D(input_shape=input_shape, filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Flatten())
-    model.add(Dropout(0.2))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(n_classes, activation='relu'))
-    model.add(Softmax())
-    return model
+# OLD TEST
+# def SCNN_diff(input_shape=(128, 128, 1)) -> models.Sequential:
+#     model = models.Sequential()
+#     model.add(Conv2D(input_shape=input_shape, filters=64, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=64, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+#     model.add(Conv2D(filters=128, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=128, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+#     model.add(Conv2D(filters=256, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=256, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=256, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+#     model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+#     model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(Conv2D(filters=512, kernel_size=(5, 5), padding="same", activation="relu"))
+#     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+#     model.add(Flatten())
+#     model.add(Dropout(0.2))
+#     model.add(Dense(256, activation='linear'))
+#     model.add(Dropout(0.2))
+#     model.add(Dense(1, activation='linear'))
+#     # model.summary()
+#     return model
+
+
+
+
--- src_nowcasting/sequence_img_generator.py
+++ src_nowcasting/sequence_img_generator.py
@@ -62,119 +62,6 @@ class DataGenerator_SCNN(Sequence):
             return X, y
 
 
-class DataGeneratorClass_SCNN(Sequence):
-
-    'Generates data for Keras'
-    def __init__(self, dataframe, batch_size=128, dim=(128, 128, 3), channel_IMG = 1, classes = 4,shuffle=False, iftest=False):
-        'Initialization'
-        self.dim = dim
-        self.batch_size = batch_size
-        self.dataframe = dataframe.reset_index(drop=True)
-        self.channel_IMG = channel_IMG
-        self.classes = classes
-        self.shuffle = shuffle
-        self.on_epoch_end()
-        self.iftest = iftest
-
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.dataframe) / self.batch_size))
-
-    def __getitem__(self, index):
-        'Generate one batch of data'
-        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
-        dataframe_temp = self.dataframe.iloc[indexes]
-        X, y = self.__data_generation(dataframe_temp)
-        return X, y
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.dataframe))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, dataframe_temp):
-        'Generates data containing batch_size samples'
-        X = np.empty((self.batch_size, *self.dim), dtype=float)
-        y = np.empty((self.batch_size), dtype=float)
-        file_name = []
-        dataframe_temp2 = dataframe_temp.reset_index(drop=True)
-        for [n_index, vector_row] in dataframe_temp2.iterrows():
-            img_row = vector_row.Image
-            value_row = vector_row.Target
-            for i_IMG in range(len(img_row)): # stack images in channels
-                img = cv2.imread(img_row[i_IMG], 0)
-                if img is None:
-                    raise ValueError(f'{img_row[i_IMG]} does not exist')
-                else:
-                    X[n_index, :, :, i_IMG] = img / 255.0
-            y[n_index] = value_row
-            file_name.append(img_row)
-        if self.iftest == True:
-            return X, to_categorical(y, num_classes=self.classes), file_name
-        elif self.iftest == False:
-            return X, to_categorical(y, num_classes=self.classes)
-
-
-class DataGeneratorDiff_SCNN(Sequence):
-    """Generates batches of images as well as their associated class labels on the fly.
-    To be passed as argument in the fit_generator function of Keras."""
-    'Generates data for Keras'
-
-    def __init__(self, dataframe, batch_size=128, dim=(128, 128, 3), channel_IMG=1, diff=2, shuffle=False, iftest=False):
-        'Initialization'
-        self.dim = dim
-        self.batch_size = batch_size
-        self.dataframe = dataframe.reset_index(drop=True)
-        self.channel_IMG = channel_IMG
-        self.diff = diff
-        self.shuffle = shuffle
-        self.on_epoch_end()
-        self.iftest = iftest
-
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.dataframe) / self.batch_size))
-
-    def __getitem__(self, index):
-        'Generate one batch of data'
-        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
-        dataframe_temp = self.dataframe.iloc[indexes]
-        X, y = self.__data_generation(dataframe_temp)
-        return X, y
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.dataframe))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, dataframe_temp):
-        'Generates data containing batch_size samples'
-        self.dim2 = (self.dim[0], self.dim[1], self.dim[-1] + self.diff)
-        X = np.empty((self.batch_size, *self.dim2), dtype=float)
-        y = np.empty((self.batch_size), dtype=float)
-        file_name = []
-        dataframe_temp2 = dataframe_temp.reset_index(drop=True)
-        for [n_index, vector_row] in dataframe_temp2.iterrows():
-            img_row = vector_row.Image
-            value_row = vector_row.Target
-            for i_IMG in range(len(img_row)-1):  # stack images in channels
-                img0 = cv2.imread(img_row[i_IMG], 0)
-                img2 = cv2.imread(img_row[i_IMG+1], 0)
-                img1 = img2-img0
-                X[n_index, :, :, i_IMG * self.diff] = img0 / 255.0
-                X[n_index, :, :, i_IMG * self.diff + 1] = img1 / 255.0
-                X[n_index, :, :, i_IMG * self.diff + 2] = img2 / 255.0
-
-            y[n_index] = value_row
-            file_name.append(img_row)
-        if self.iftest == True:
-            return X, y, file_name
-        elif self.iftest == False:
-            return X, y
-
-
 class DataGeneratorGHI_SCNN(Sequence):
 
     'Generates data for Keras'
@@ -238,3 +125,65 @@ class DataGeneratorGHI_SCNN(Sequence):
             return X, y, file_name
         elif self.iftest == False:
             return X, y
+
+
+# OLD TEST
+# class DataGeneratorDiff_SCNN(Sequence):
+#     """Generates batches of images as well as their associated class labels on the fly.
+#     To be passed as argument in the fit_generator function of Keras."""
+#     'Generates data for Keras'
+
+#     def __init__(self, dataframe, batch_size=128, dim=(128, 128, 3), channel_IMG=1, diff=2, shuffle=False, iftest=False):
+#         'Initialization'
+#         self.dim = dim
+#         self.batch_size = batch_size
+#         self.dataframe = dataframe.reset_index(drop=True)
+#         self.channel_IMG = channel_IMG
+#         self.diff = diff
+#         self.shuffle = shuffle
+#         self.on_epoch_end()
+#         self.iftest = iftest
+
+#     def __len__(self):
+#         'Denotes the number of batches per epoch'
+#         return int(np.floor(len(self.dataframe) / self.batch_size))
+
+#     def __getitem__(self, index):
+#         'Generate one batch of data'
+#         indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+#         dataframe_temp = self.dataframe.iloc[indexes]
+#         X, y = self.__data_generation(dataframe_temp)
+#         return X, y
+
+#     def on_epoch_end(self):
+#         'Updates indexes after each epoch'
+#         self.indexes = np.arange(len(self.dataframe))
+#         if self.shuffle == True:
+#             np.random.shuffle(self.indexes)
+
+#     def __data_generation(self, dataframe_temp):
+#         'Generates data containing batch_size samples'
+#         self.dim2 = (self.dim[0], self.dim[1], self.dim[-1] + self.diff)
+#         X = np.empty((self.batch_size, *self.dim2), dtype=float)
+#         y = np.empty((self.batch_size), dtype=float)
+#         file_name = []
+#         dataframe_temp2 = dataframe_temp.reset_index(drop=True)
+#         for [n_index, vector_row] in dataframe_temp2.iterrows():
+#             img_row = vector_row.Image
+#             value_row = vector_row.Target
+#             for i_IMG in range(len(img_row)-1):  # stack images in channels
+#                 img0 = cv2.imread(img_row[i_IMG], 0)
+#                 img2 = cv2.imread(img_row[i_IMG+1], 0)
+#                 img1 = img2-img0
+#                 X[n_index, :, :, i_IMG * self.diff] = img0 / 255.0
+#                 X[n_index, :, :, i_IMG * self.diff + 1] = img1 / 255.0
+#                 X[n_index, :, :, i_IMG * self.diff + 2] = img2 / 255.0
+
+#             y[n_index] = value_row
+#             file_name.append(img_row)
+#         if self.iftest == True:
+#             return X, y, file_name
+#         elif self.iftest == False:
+#             return X, y
+
+
